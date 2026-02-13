326493a79d41b384c507c0c51ac22f315e54a96c
Sam Sudhan Elambegan <samsudhan@gmail.com>
2026-02-13 13:21:47 +0530

    Initial commit

---
+++ 01_basics_llm.js
@@ -0,0 +1,8 @@
+
+//console.log("This is a JavaScript file. This is playwrightclass");
+
+console.log("process.platform: " + process.platform);
+
+console.log(process.version);
+
+console.log("process.arch: " + process.arch);
