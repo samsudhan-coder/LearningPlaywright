5216610ca239af5e98b5ce058bbfd5de6728c16e
Sam Sudhan Elambegan <samsudhan@gmail.com>
2026-02-14 08:29:17 +0530

    chore: commit workspace changes

---
+++ CoreConcepts.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Author:Sam SUdhan
+ * Date: 14-02-2026
+ * /
+ * 
+ * 
+ */
+//console.log("This is a JavaScript file. This is CoreConcepts.js");
+
+//identifier
+
+var name = "Sam Sudhan";
+console.log(name);
+
+function testfunc(){
+
+    console.log("Test Funciton");
+}
+
+testfunc();
