32036f3c88f8e475b2f239bbe852d86f7538cb04
Sam Sudhan Elambegan <samsudhan@gmail.com>
2026-02-17 08:49:12 +0530

    Save editor changes

--- CoreConcepts.js
+++ CoreConcepts.js
@@ -10,12 +10,23 @@
 
 //identifier
 
-var name = "Sam Sudhan";
-console.log(name);
+let fn = "Sam Sudhan";
+// let ln = "Elambegan"
+console.log(fn);
 
 function testfunc(){
-
-    console.log("Test Funciton");
+    //console.log(fn);
+   let fn = "only Sam";
+  // let fn = "testname";
+    //console.log(fn);
+    if(true){
+        //console.log(fn);
+        let fn="testname";
+       console.log(fn);
+    }
+    console.log(fn);
 }
 
 testfunc();
+
+console.log(fn);
