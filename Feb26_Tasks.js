03770c8972a1cf3c90e7f4b4520c2255c5a8c1f5
Sam Sudhan Elambegan <samsudhan@gmail.com>
2026-02-28 06:54:17 +0530

    Save workspace changes: automatic push from Copilot

---
+++ Feb26_Tasks.js
@@ -0,0 +1,22 @@
+
+
+// for(let i = 1; i < 50; i++) {
+//     if(i%2===0)
+//         console.log("Even number: " + i);    
+// }
+
+// let ii=1;
+// do{
+//     if(ii%2===0)
+//         console.log("Even number: " + ii);
+
+//     ii++;
+// }
+// while(ii<=100);
+
+let yy=1;
+while(yy<=100){
+    if(yy%2===0)
+        console.log("Even number: " + yy);
+    yy++;
+}
